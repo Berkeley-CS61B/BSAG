d6d6157bd6ca8f50682d369961fbaca9c030332d
Ethan Ordentlich <ethan.ordentlich2017@gmail.com>
2024-08-06 16:58:42 -0500

    Export `main` as shown in README

--- bsag/__init__.py
+++ bsag/__init__.py
@@ -1,10 +1,12 @@
 from . import bsagio, plugin
 from ._types import BaseStepConfig, BaseStepDefinition, ParamBaseStep
+from .bsag import main
 
 __all__ = [
     "BaseStepConfig",
     "BaseStepDefinition",
     "ParamBaseStep",
     "bsagio",
+    "main",
     "plugin",
 ]
